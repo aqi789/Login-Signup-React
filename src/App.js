fc0dbfd0e1e7e8fdcf59c0d9ba8c49f5423a2821
aqi789 <aqilaaslam2002@gmail.com>
2023-12-21 08:17:00 +0530

    Login-Signup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import LoginSignup from './Components/LoginSignup/LoginSignup';  
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+      {/* Other components or content */}
+    </div>
+  );
+}
+
+export default App;
